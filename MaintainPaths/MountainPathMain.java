fd9600016eba3d7ccaeab81d0a5e695cbbccbd95
Pandabo001 <15621.smnr@gmail.com>
2017-05-01 21:06:50 +0700

    Add files via upload

--- MaintainPaths/MountainPathMain.java
+++ MaintainPaths/MountainPathMain.java
@@ -24,6 +24,26 @@ public class MountainPathMain{
       System.out.println(map.grid[i][0]);
     }
     
+    int minRow = map.indexOfMinInCol(0);
+    System.out.println("Row with lowest value in col 0: " + minRow);
+    
+    //Step 3 - draw the map
+    map.drawMap(g);
+    
+    //Step 4 - draw a greedy path
+    g.setColor(Color.RED); //can set the color of the 'brush' before drawing, then method doesn't need to worry about it
+    int totalChange = map.drawLowestElevPath(g, minRow); //use minRow from Step 2 as starting point
+    System.out.println("Lowest-Elevation-Change Path starting at row " + minRow + " gives total change of: " + totalChange);
+    
+    //Step 5 - draw the best path
+    g.setColor(Color.RED);
+    int bestRow = map.indexOfLowestElevPath(g);
+    
+    //map.drawMap(g); //use this to get rid of all red lines
+    g.setColor(Color.GREEN); //set brush to green for drawing best path
+    totalChange = map.drawLowestElevPath(g, bestRow);
+    System.out.println("The Lowest-Elevation-Change Path starts at row: " + bestRow + " and gives a total change of: " + totalChange);
+    
   }
   
   
